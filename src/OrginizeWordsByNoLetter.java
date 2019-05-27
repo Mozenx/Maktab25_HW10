7e55feffb10c39d69f33f81450847ad6812e6251
Mohsen <mim_z@hotmail.com>
2019-05-27 10:17:31 +0430

    add main method and declear list of string and initial it

--- src/OrginizeWordsByNoLetter.java
+++ src/OrginizeWordsByNoLetter.java
@@ -1,2 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
 public class OrginizeWordsByNoLetter {
+
+    public static void main(String[] args) {
+
+        List<String > names = List.of("Amir", "Hatef", "Mehran", "Mojtaba", "Mohammad", "Ali", "Davood", "Reza", "Mohsen");
+
+
+        
+
+
+    }
 }
